31f1fb75b99a3eab7c59b1f3282c09859e9c1711
Angela Balaguer <balaguer.valles@gmail.com>
2020-01-09 19:09:20 -0500

    implementando un RecyclerView

---
+++ app/src/main/java/com/acrispin/u1tema2/InterfaceToast.java
@@ -0,0 +1,6 @@
+package com.acrispin.u1tema2;
+
+public interface InterfaceToast {
+    void mensaje(String mensaje);
+}
+
--- app/src/main/java/com/acrispin/u1tema2/MainActivity.java
+++ app/src/main/java/com/acrispin/u1tema2/MainActivity.java
@@ -45,6 +45,10 @@ public class MainActivity extends AppCompatActivity {
             startActivity(new Intent(this, Permisos.class));
             return true;
         }
+        if (id == R.id.activity_mirecyclerview) {
+            startActivity(new Intent(this, MiRecyclerView.class));
+            return true;
+        }
         return super.onOptionsItemSelected(item);
     }
 
